1df6af464b7356095241807e27b944a404954122
hldh214 <hldh214@gmail.com>
2024-06-19 16:33:39 +0900

    Remove 'app.js' and 'welcome.blade.php' files
    These files are deleted from resources/js and resources/views respectively. This is done as part of a clean-up and simplification process, as they are no longer needed.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/auth/github/redirect', function () {
-    return Socialite::driver('github')->redirect();
-});
-
-Route::get('/auth/github/callback', function () {
-    $githubUser = Socialite::driver('github')->user();
-
-    dd($githubUser);
-
-    $user = User::updateOrCreate([
-        'email' => $githubUser->email,
-    ], [
-        'name' => $githubUser->name,
-        'email' => $githubUser->email,
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-
-    Auth::login($user);
-
-    return redirect('/dashboard');
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
