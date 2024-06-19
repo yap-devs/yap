f8d607c5017993bd368f726718c6d8c403e6fc11
hldh214 <hldh214@gmail.com>
2024-06-19 16:58:45 +0900

    Remove 'app.js' and 'welcome.blade.php' files
    These files are deleted from resources/js and resources/views respectively. This is done as part of a clean-up and simplification process, as they are no longer needed.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// telemetry
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
