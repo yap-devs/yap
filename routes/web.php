96dc97a0e5b0bf58614efd9f98979c4bbabed22e
hldh214 <hldh214@gmail.com>
2024-06-19 16:11:05 +0900

    Add GitHub auth config, update dependencies, and ignore IDE files
    Added GitHub OAuth configuration into services. Updated dependencies in composer.json and composer.lock. Also, added IDE-specific files such as .phpstorm.meta.php and _ide_helper_models.php to .gitignore.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    dd($githubUser);
+
+    $user = User::updateOrCreate([
+        'email' => $githubUser->email,
+    ], [
+        'name' => $githubUser->name,
+        'email' => $githubUser->email,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+});
+
