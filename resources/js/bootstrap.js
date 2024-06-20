37cfa2542308a9ba09da9d68004fba297d803a09
hldh214 <hldh214@gmail.com>
2024-06-20 09:22:53 +0900

    Refactor code to improve code style consistency
    This refactor involves multiple files across the application - dealing with indentations and improvements in the code style for better clarity and readability. Changes primarily cover the format of object destructuring, improved formatting in function declarations, and some code syntax refinements. No functionality changes have been made.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
+// telemetry
+import * as Sentry from "@sentry/react";
 
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// telemetry
-import * as Sentry from "@sentry/react";
-
 Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+  dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
 });
