600bf8dfeaf49a0697c53f204aae0bf59c7919c0
hldh214 <hldh214@gmail.com>
2024-06-26 14:08:20 +0900

    Implement V2ray user management and update dependencies
    The V2rayService class has been expanded to include methods for adding and removing users. This is implemented using Laravel's Process Facade. Moreover, unnecessary dependencies (ext-grpc, ext-protobuf, google/protobuf, grpc/grpc, spatie/ssh) in the composer.json and composer.lock files have been removed. Also added a new configuration file at `config/v2ray.php` for customizing the V2bridge path.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- app/Services/V2rayService.php
+++ app/Services/V2rayService.php
@@ -3,7 +3,30 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\Process;
+
 class V2rayService
 {
+    private string $prefix = '';
 
+    public function __construct(
+        private readonly string $server
+    )
+    {
+        $this->prefix = config('v2ray.v2bridge.path') . ' handler -s ' . $this->server;
+    }
+
+    public function addUser($email, $uuid)
+    {
+        $result = Process::run($this->prefix . ' addV2rayVmessUser -e ' . $email . ' -u ' . $uuid);
+
+        return json_decode($result->output(), true);
+    }
+
+    public function removeUser($email)
+    {
+        $result = Process::run($this->prefix . ' removeV2rayUser -e ' . $email);
+
+        return json_decode($result->output(), true);
+    }
 }
---
+++ config/v2ray.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'v2bridge' => [
+        'path' => env('V2BRIDGE_PATH', app()->basePath('v2bridge/v2bridge')),
+    ]
+];
