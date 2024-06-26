ddd3c02d66ed2cd314c1e8eec195f38e1e8c1fac
hldh214 <hldh214@gmail.com>
2024-06-26 17:53:47 +0900

    Replace Server model with VmessServer model
    The Server model and corresponding migration has been removed and replaced with a more detailed VmessServer model along with a new corresponding migration file. The user's balance has also been added to display with the user name in the AuthenticatedLayout.jsx file. Furthermore, 'seeders/PrivateSeeder.php' has been added to .gitignore.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

---
+++ database/migrations/2024_06_26_160818_create_vmess_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vmess_servers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('The name of the server');
+            $table->string('server')->unique()->comment('The server address for the clash config `server` field');
+            $table->unsignedSmallInteger('port')->comment('The port of the server for clash config `port` field');
+            $table->unsignedTinyInteger('rate')->default(1)->comment('The rate of the server');
+            $table->string('internal_server')->default('')->comment('Internal server address for the v2bridge');
+            $table->unsignedBigInteger('uptime')->default(0)->comment('The uptime of the server in seconds');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vmess_servers');
+    }
+};
