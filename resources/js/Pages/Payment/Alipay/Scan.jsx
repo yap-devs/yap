c0b9072c365ef68d71412a2e5a1d09536abdb0c8
hldh214 <hldh214@gmail.com>
2024-07-11 13:59:02 +0900

    Refactor Alipay scan-to-pay UI and simplify trade status rendering
    The commit simplifies the way trade statuses are handled and displayed in the Alipay Scan-to-Pay feature, moving away from function render calls to directly using object properties. Additionally, the layout of the Scan Component have been refactored to be more streamlined and efficient. This change should make the code more readable and maintainable.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/Pages/Payment/Alipay/Scan.jsx
+++ resources/js/Pages/Payment/Alipay/Scan.jsx
@@ -1,35 +1,15 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head, router} from '@inertiajs/react';
+import {router} from '@inertiajs/react';
 import {QRCodeCanvas} from "qrcode.react";
 import axios from "axios";
 import {useEffect, useState} from 'react';
 
-export default function Stat({auth, QRInfo, amount, paymentId}) {
+export default function Scan({_auth, QRInfo, amount, paymentId}) {
   const [tradeStatus, setTradeStatus] = useState('');
-
-  const renderTradeStatus = () => {
-    if (tradeStatus === '') return;
-    if (tradeStatus === 'WAIT_BUYER_PAY') {
-      return (
-        <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4">
-          <p className="font-bold">Waiting for payment...</p>
-        </div>
-      );
-    }
-    if (tradeStatus === 'TRADE_SUCCESS') {
-      return (
-        <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4">
-          <p className="font-bold">Payment success! Redirecting...</p>
-        </div>
-      );
-    }
-    if (tradeStatus === 'TRADE_CLOSED') {
-      return (
-        <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-          <p className="font-bold">Payment closed!</p>
-        </div>
-      );
-    }
+  const tradeStatusMessage = {
+    '': {text: 'Scan the QR code to pay', color: 'bg-gray-500 text-white'},
+    'WAIT_BUYER_PAY': {text: 'Scanned, awaiting pay...', color: 'bg-blue-500 text-white'},
+    'TRADE_SUCCESS': {text: 'Paid! Redirecting...', color: 'bg-green-500 text-white'},
+    'TRADE_CLOSED': {text: 'Payment closed!', color: 'bg-red-500 text-white'}
   };
 
   useEffect(() => {
@@ -54,31 +34,24 @@ export default function Stat({auth, QRInfo, amount, paymentId}) {
   }, []);
 
   return (
-    <AuthenticatedLayout
-      user={auth.user}
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Scan to Pay</h2>}
-    >
-      <Head title="Scan to Pay"/>
-
-      <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">
-              <div className="text-2xl font-semibold">Scan to Pay</div>
-              <div className="mt-4">
-                <div className="text-lg">Amount: ${amount}</div>
-                <div className="text-lg">Order ID: {QRInfo['out_trade_no']}</div>
-                <div className="mt-4">
-                  <QRCodeCanvas value={QRInfo['qr_code']} size={256}/>
-                </div>
-              </div>
-              <div className="mt-4">
-                {renderTradeStatus()}
-              </div>
-            </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
+      <div className="p-4 bg-white rounded shadow-lg">
+        <div className="items-center">
+          <div
+            className={`px-4 py-2 rounded-md text-2xl font-semibold text-center shadow-md ${tradeStatusMessage[tradeStatus].color}`}
+          >
+            {tradeStatusMessage[tradeStatus].text}
+          </div>
+          <div className="mt-4">
+            <div className="px-4 py-2 bg-gray-800 text-white rounded-md text-lg">Payment Information</div>
+            <div className="px-4 py-2 mt-2 bg-gray-200 text-black rounded-md">Amount: ${amount}</div>
+            <div className="px-4 py-2 mt-2 bg-gray-200 text-black rounded-md">Order ID: {QRInfo['out_trade_no']}</div>
+          </div>
+          <div className="mt-4">
+            <QRCodeCanvas value={QRInfo['qr_code']} size={256}/>
           </div>
         </div>
       </div>
-    </AuthenticatedLayout>
+    </div>
   );
 }
