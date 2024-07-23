7a435d46ad650deac9832d999e03ed27c948bd54
hldh214 <hldh214@gmail.com>
2024-07-23 11:43:48 +0900

    Add client download section to customer service page
    A new section for client download has been added at the top of the customer service page. It includes download links for different OSs (Windows, MacOS, Linux, and Android). Some styles and structures of existing content have also been slightly updated for better clarity and organization.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/Pages/CustomerService/Index.jsx
+++ resources/js/Pages/CustomerService/Index.jsx
@@ -13,22 +13,36 @@ export default function Index({auth}) {
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div className="p-6 text-gray-900">
-            <div className="flex items-center justify-between">
-              <h2 className="text-lg font-semibold text-gray-800">Customer Service</h2>
+            {/*Client Download section */}
+            <div className="text-gray-900 pl-6 pb-6 pt-8 rounded shadow mb-6 bg-blue-50">
+              <h2 className="text-lg font-semibold text-gray-800">Client Download</h2>
+              <p className="text-sm text-gray-700 mt-2">
+                Download the latest version of the client software.
+              </p>
+              <div className="underline mt-2 space-x-4">
+                <a href="https://github.com/clash-verge-rev/clash-verge-rev/releases/latest" target="_blank"
+                   rel="noreferrer noopener">
+                  For Windows && MacOS && Linux
+                </a>
+                <a href="https://github.com/MetaCubeX/ClashMetaForAndroid/releases/latest" target="_blank"
+                   rel="noreferrer noopener">
+                  For Android
+                </a>
+              </div>
             </div>
-            <div className="mt-6">
-              <p className="text-sm text-gray-700">
+
+            {/*Customer Service section */}
+            <div className="text-gray-900 pl-6 pb-6 pt-8 rounded shadow mb-6 bg-blue-50">
+              <h2 className="text-lg font-semibold text-gray-800">Customer Service</h2>
+              <p className="text-sm text-gray-700 mt-2">
                 If you have any questions or concerns, please feel free to contact us.
               </p>
               <p className="text-sm text-gray-700">
                 We will respond to your inquiries as soon as possible.
               </p>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-semibold text-gray-800">Contact Information</h3>
-              <p className="text-sm text-gray-700">
+              <div className="mt-2 space-x-4">
                 Telegram: <a href="https://t.me/yap_devs" target="_blank" rel="noreferrer noopener">@yap_devs</a>
-              </p>
+              </div>
             </div>
           </div>
         </div>
