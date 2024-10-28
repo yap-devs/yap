74b6643e05284939f52bccf2d3c3a93e01d8bc90
hldh214 <hldh214@gmail.com>
2024-10-28 15:28:32 +0900

    Simplify JSX code structure
    Refactor the JSX code for better readability and organization by removing redundant lines and adding concise formatting. This does not change any functionality but makes the code much easier to maintain.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/Pages/Package/Index.jsx
+++ resources/js/Pages/Package/Index.jsx
@@ -22,152 +22,89 @@ export default function Index({auth, packages, userPackages}) {
     setConfirmingBuy(false);
   }
 
-  return (<AuthenticatedLayout
-    user={auth.user}
-    header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">
-      Package
-    </h2>}
-  >
-    <Head title="Package"/>
-    <div className="py-12">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        {
-          errors.error &&
-          <div className="p-4 sm:p-8 bg-red-600 bg-opacity-10 text-red-600 rounded-lg">
-            <div className="flex items-center">
-              <svg className="h-6 w-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                   xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"/>
-              </svg>
-              <span className="ml-2">{errors.error}</span>
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Package</h2>}
+    >
+      <Head title="Package"/>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          {errors.error && (
+            <div className="p-4 sm:p-8 bg-red-600 bg-opacity-10 text-red-600 rounded-lg">
+              <div className="flex items-center">
+                <svg className="h-6 w-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+                <span className="ml-2">{errors.error}</span>
+              </div>
             </div>
-          </div>
-        }
-        {
-          errors.success &&
-          <div className="p-4 sm:p-8 bg-green-600 bg-opacity-10 text-green-600 rounded-lg">
-            <div className="flex items-center">
-              <svg className="h-6 w-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                   xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                      d="M5 13l4 4L19 7"/>
-              </svg>
-              <span className="ml-2">{errors.success}</span>
+          )}
+          {errors.success && (
+            <div className="p-4 sm:p-8 bg-green-600 bg-opacity-10 text-green-600 rounded-lg">
+              <div className="flex items-center">
+                <svg className="h-6 w-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/>
+                </svg>
+                <span className="ml-2">{errors.success}</span>
+              </div>
             </div>
-          </div>
-        }
-        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          <div className="p-6 text-gray-900" style={{overflowX: 'auto'}}>
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead>
-              <tr>
-                <th
-                  className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Name
-                </th>
-                <th
-                  className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Description
-                </th>
-                <th
-                  className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Traffic
-                </th>
-                <th
-                  className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Price
-                </th>
-                <th
-                  className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Validity Period (days)
-                </th>
-                <th className="px-6 py-3 bg-gray-50"></th>
-              </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-              {packages.map((_package) => (
-                <tr key={_package.id}>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">{_package.name}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">{_package.description}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">{formatBytes(_package.traffic_limit)}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">
-                      <span className="line-through text-gray-500 mr-1">{formatPrice(_package.original_price)}</span>
-                      <span className="text-green-600 font-bold text-lg">{formatPrice(_package.price)}</span>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">{_package.duration_days}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                    <div className="text-sm leading-5 text-gray-900">
-                      <SecondaryButton
-                        onClick={() => setConfirmingBuy(_package.id)}
-                        className="mt-4 bg-gradient-to-r from-red-500 to-yellow-500 hover:from-yellow-500 hover:to-red-500 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
-                      >
-                        Buy
-                      </SecondaryButton>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="mt-6">
-          <h2 className="text-lg font-medium text-gray-900">
-            Your Packages
-          </h2>
+          )}
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900" style={{overflowX: 'auto'}}>
               <table className="min-w-full divide-y divide-gray-200">
                 <thead>
                 <tr>
                   <th
-                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Name
+                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name
                   </th>
                   <th
-                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Traffic(Remain/Total)
+                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Description
                   </th>
                   <th
-                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Status
+                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Traffic
                   </th>
                   <th
-                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Expired At
+                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Price
                   </th>
+                  <th
+                    className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Validity
+                    Period (days)
+                  </th>
+                  <th className="px-6 py-3 bg-gray-50"></th>
                 </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                {userPackages.map((userPackage) => (
-                  <tr key={userPackage.id}>
+                {packages.map((_package) => (
+                  <tr key={_package.id} className="hover:bg-yellow-50 transition duration-300 ease-in-out">
                     <td className="px-6 py-4 whitespace-no-wrap">
-                      <div className="text-sm leading-5 text-gray-900">{userPackage.package.name}</div>
+                      <div className="text-sm leading-5 text-gray-900">{_package.name}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="text-sm leading-5 text-gray-900">{_package.description}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <div className="text-sm leading-5 text-gray-900">{formatBytes(_package.traffic_limit)}</div>
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap">
                       <div className="text-sm leading-5 text-gray-900">
-                        {formatBytes(userPackage.remaining_traffic)}/{formatBytes(userPackage.package.traffic_limit)}
+                        <span className="line-through text-gray-500 mr-1">{formatPrice(_package.original_price)}</span>
+                        <span className="text-green-600 font-bold text-lg">{formatPrice(_package.price)}</span>
                       </div>
                     </td>
                     <td className="px-6 py-4 whitespace-no-wrap">
-                      <div className="text-sm leading-5 text-gray-900">
-                        {userPackage.status}
-                      </div>
+                      <div className="text-sm leading-5 text-gray-900">{_package.duration_days}</div>
                     </td>
-                    <td className="px-6 py-4 whitespace-no-wrap">
+                    <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                       <div className="text-sm leading-5 text-gray-900">
-                        {userPackage.ended_at}
+                        <SecondaryButton
+                          onClick={() => setConfirmingBuy(_package.id)}
+                          className="mt-4 bg-gradient-to-r from-red-500 to-yellow-500 hover:from-yellow-500 hover:to-red-500 text-white font-bold py-2 px-4 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
+                        >
+                          🚀 Buy Now!
+                        </SecondaryButton>
                       </div>
                     </td>
                   </tr>
@@ -176,28 +113,67 @@ export default function Index({auth, packages, userPackages}) {
               </table>
             </div>
           </div>
+          <div className="mt-6">
+            <h2 className="text-lg font-medium text-gray-900">Your Packages</h2>
+            <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+              <div className="p-6 text-gray-900" style={{overflowX: 'auto'}}>
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead>
+                  <tr>
+                    <th
+                      className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name
+                    </th>
+                    <th
+                      className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Traffic
+                      (Remain/Total)
+                    </th>
+                    <th
+                      className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Status
+                    </th>
+                    <th
+                      className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Expired
+                      At
+                    </th>
+                  </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                  {userPackages.map((userPackage) => (
+                    <tr key={userPackage.id}>
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        <div className="text-sm leading-5 text-gray-900">{userPackage.package.name}</div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        <div className="text-sm leading-5 text-gray-900">
+                          {formatBytes(userPackage.remaining_traffic)}/{formatBytes(userPackage.package.traffic_limit)}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        <div className="text-sm leading-5 text-gray-900">{userPackage.status}</div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap">
+                        <div className="text-sm leading-5 text-gray-900">{userPackage.ended_at}</div>
+                      </td>
+                    </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <Modal show={confirmingBuy} onClose={() => setConfirmingBuy(false)}>
-      <form className="p-6" onSubmit={handleSubmit}>
-        <h2 className="text-lg font-medium text-gray-900">
-          Are you sure you want to buy this package?
-        </h2>
-
-        <p className="mt-1 text-sm text-gray-600">
-          Buying this package will cost you ${packages.find(p => p.id === confirmingBuy)?.price}.
-        </p>
-
-        <div className="mt-6 flex justify-end">
-          <SecondaryButton onClick={() => setConfirmingBuy(false)}>Cancel</SecondaryButton>
-
-          <PrimaryButton className="ms-3">
-            Buy
-          </PrimaryButton>
-        </div>
-      </form>
-    </Modal>
-  </AuthenticatedLayout>);
+      <Modal show={confirmingBuy} onClose={() => setConfirmingBuy(false)}>
+        <form className="p-6" onSubmit={handleSubmit}>
+          <h2 className="text-lg font-medium text-gray-900">Are you sure you want to buy this package?</h2>
+          <p className="mt-1 text-sm text-gray-600">Buying this package will cost you
+            ${packages.find(p => p.id === confirmingBuy)?.price}.</p>
+          <div className="mt-6 flex justify-end">
+            <SecondaryButton onClick={() => setConfirmingBuy(false)}>Cancel</SecondaryButton>
+            <PrimaryButton className="ms-3">Buy</PrimaryButton>
+          </div>
+        </form>
+      </Modal>
+    </AuthenticatedLayout>
+  );
 }
