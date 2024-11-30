89a48632d92cb962bc381006479f0e8b649927a5
hldh214 <hldh214@gmail.com>
2024-11-30 14:02:11 +0900

    Update USD to RMB exchange rate in yap config
    The exchange rate for USD to RMB in the yap configuration has been updated from 7.4 to 7.3. This change reflects more current currency exchange rates to ensure accurate payment calculations.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- config/yap.php
+++ config/yap.php
@@ -3,7 +3,7 @@
 return [
     'unit_price' => env('YAP_UNIT_PRICE', 0.02),  // 0.02 USD per GB
     'payment' => [
-        'usd_rmb_rate' => env('YAP_USD_RMB_RATE', 7.4),
+        'usd_rmb_rate' => env('YAP_USD_RMB_RATE', 7.3),
         'alipay' => [
             'subject' => env('ALIPAY_SUBJECT', 'Yap Donation'),
         ]
