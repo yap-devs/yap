47d1cd7b8e634f2bf10db79f0feb409b89f66445
hldh214 <hldh214@gmail.com>
2024-11-30 09:54:32 +0900

    Update SQL query to dynamically fetch today's traffic report
    Modified the SQL query to retrieve traffic data for the current day instead of a hardcoded date. This change ensures that the traffic report is always up-to-date without requiring manual updates to the query date each day.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- database/console.sql
+++ database/console.sql
@@ -16,14 +16,15 @@ where payments.status = 'paid'
 group by month
 order by month desc;
 
--- specific day traffic report
+-- today traffic report
 select date_format(user_stats.created_at, '%Y-%m-%d')                                              as day,
        user_id,
        users.name,
        round(sum(user_stats.traffic_downlink + user_stats.traffic_uplink) / 1024 / 1024 / 1024, 2) as daily_traffic_gb
 from user_stats
          inner join users on user_stats.user_id = users.id
-where date_format(user_stats.created_at, '%Y-%m-%d') = '2024-11-26'
+# where date_format(user_stats.created_at, '%Y-%m-%d') = '2024-11-26'
+where date_format(user_stats.created_at, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')
 group by user_id, day
 order by daily_traffic_gb desc;
 
