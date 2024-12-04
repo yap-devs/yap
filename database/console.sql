751690c2ce985581b110c29d6483ee8f9461547a
hldh214 <hldh214@gmail.com>
2024-12-04 09:15:46 +0900

    Add yesterday traffic data to report
    Updated the SQL query to include traffic statistics from both yesterday and today. This includes an adjustment in the 'where' clause to filter data accordingly and an order change to prioritize sorting by day first.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- database/console.sql
+++ database/console.sql
@@ -16,17 +16,17 @@ where payments.status = 'paid'
 group by month
 order by month desc;
 
--- today traffic report
+-- yesterday and today traffic report
 select date_format(user_stats.created_at, '%Y-%m-%d')                                              as day,
        user_id,
        users.name,
        round(sum(user_stats.traffic_downlink + user_stats.traffic_uplink) / 1024 / 1024 / 1024, 2) as daily_traffic_gb
 from user_stats
          inner join users on user_stats.user_id = users.id
-# where date_format(user_stats.created_at, '%Y-%m-%d') = '2024-11-26'
 where date_format(user_stats.created_at, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')
+or date_format(user_stats.created_at, '%Y-%m-%d') = date_format(date_sub(now(), interval 1 day), '%Y-%m-%d')
 group by user_id, day
-order by daily_traffic_gb desc;
+order by day, daily_traffic_gb desc;
 
 -- user active package report
 select user_packages.id,
