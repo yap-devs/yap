2fa189948b595f24986f5b1271245c0965ccd657
hldh214 <hldh214@gmail.com>
2024-12-05 13:52:24 +0900

    Add monthly cost report query to console.sql
    A new SQL query for generating monthly cost reports has been added, which calculates the total negative balance amounts per month for users with IDs greater than 5. This addition aims to enhance financial analysis capabilities within the application by providing an overview of monthly expenses. Additionally, minor formatting adjustments were made to improve code readability.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- database/console.sql
+++ database/console.sql
@@ -16,6 +16,15 @@ where payments.status = 'paid'
 group by month
 order by month desc;
 
+-- monthly cost report
+select date_format(balance_details.created_at, '%Y-%m') as month,
+       sum(balance_details.amount)                      as monthly_cost
+from balance_details
+where balance_details.amount < 0
+  and user_id > 5
+group by month
+order by month desc;
+
 -- yesterday and today traffic report
 select date_format(user_stats.created_at, '%Y-%m-%d')                                              as day,
        user_id,
@@ -24,7 +33,7 @@ select date_format(user_stats.created_at, '%Y-%m-%d')                           
 from user_stats
          inner join users on user_stats.user_id = users.id
 where date_format(user_stats.created_at, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')
-or date_format(user_stats.created_at, '%Y-%m-%d') = date_format(date_sub(now(), interval 1 day), '%Y-%m-%d')
+   or date_format(user_stats.created_at, '%Y-%m-%d') = date_format(date_sub(now(), interval 1 day), '%Y-%m-%d')
 group by user_id, day
 order by day, daily_traffic_gb desc;
 
@@ -34,8 +43,9 @@ select user_packages.id,
        packages.name,
        user_packages.ended_at,
        round(user_packages.remaining_traffic / 1024 / 1024 / 1024, 2) as remaining_gb,
-       round(packages.traffic_limit / 1024 / 1024 / 1024, 2) as total_gb
+       round(packages.traffic_limit / 1024 / 1024 / 1024, 2)          as total_gb
 from users
          inner join user_packages on users.id = user_packages.user_id
          inner join packages on user_packages.package_id = packages.id
-where user_packages.status = 'active' and users.id > 5;
+where user_packages.status = 'active'
+  and users.id > 5;
