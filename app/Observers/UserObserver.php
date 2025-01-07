868d09a3da045c2a1aac2f69907ab85567a87806
hldh214 <hldh214@gmail.com>
2025-01-07 16:05:00 +0900

    Throttle balance reminder notifications with caching
    Added a caching mechanism to prevent frequent balance reminders. Notifications are now throttled by a 7-day debounce period to avoid spamming users. This ensures a better user experience while maintaining notification relevance.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,9 +4,12 @@ namespace App\Observers;
 
 use App\Models\User;
 use App\Notifications\BalanceReminder;
+use Illuminate\Support\Facades\Cache;
 
 class UserObserver
 {
+    const BR_DEBOUNCE_DAYS = 7;
+
     /**
      * Handle the User "updating" event.
      */
@@ -16,8 +19,15 @@ class UserObserver
         if ($user->isDirty('balance') && $user->balance < $user->getOriginal('balance')) {
             $user->last_settled_at = now();
 
-            if ($user->balance < config('yap.balance_reminder_threshold') && now()->hour === 9) {
+            if ($user->balance < config('yap.balance_reminder_threshold')) {
+                $br_last_sent_at = Cache::get('balance_reminder_last_sent_at_' . $user->id);
+
+                if ($br_last_sent_at && now()->diffInDays($br_last_sent_at) < self::BR_DEBOUNCE_DAYS) {
+                    return;
+                }
+
                 $user->notify(new BalanceReminder($user));
+                Cache::put('balance_reminder_last_sent_at_' . $user->id, now(), now()->addDays(self::BR_DEBOUNCE_DAYS));
             }
         }
     }
