05a3908ee5a0d645776485ce2db28f1c5d6fcdd5
hldh214 <hldh214@gmail.com>
2025-01-14 13:31:56 +0900

    Add loop to continuously check stock availability
    Previously, the script checked stock availability only once. This update introduces an infinite loop, rechecking every 10 seconds until the target is in stock, at which point the script exits. This ensures real-time monitoring of stock status.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- alice.sh
+++ alice.sh
@@ -4,11 +4,15 @@ set -e
 
 TARGET_NAME=${1:-"HK.Contract.Basic.M1"}
 
-res=$(curl -s --cookie 'lang=' 'https://app.alice.ws/api/product/product?action=regionToproduct&id=1')
-stock=$(echo $res | jq -r ".[] | select(.name == \"$TARGET_NAME\") | .stock")
+while [ : ]; do
+    res=$(curl -s --cookie 'lang=' 'https://app.alice.ws/api/product/product?action=regionToproduct&id=1')
+    stock=$(echo $res | jq -r ".[] | select(.name == \"$TARGET_NAME\") | .stock")
 
-if [ $stock -gt 0 ]; then
-    printf "\033[0;32m%(%Y-%m-%d %H:%M:%S)T %s: in stock => %d\033[0m\n" -1 $TARGET_NAME $stock
-else
-    printf "\033[0;31m%(%Y-%m-%d %H:%M:%S)T %s: out of stock\033[0m\n" -1 $TARGET_NAME
-fi
+    if [ $stock -gt 0 ]; then
+        printf "\033[0;32m%(%Y-%m-%d %H:%M:%S)T %s: in stock => %d\033[0m\n" -1 $TARGET_NAME $stock
+        exit 0
+    else
+        printf "\033[0;31m%(%Y-%m-%d %H:%M:%S)T %s: out of stock\033[0m\n" -1 $TARGET_NAME
+        sleep 10
+    fi
+done
