46cc6f41f03d04cf43e1018edef7df0517bff734
hldh214 <hldh214@gmail.com>
2025-03-12 15:34:16 +0900

    Remove unused SVG icon from Dashboard header
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -169,9 +169,6 @@ export default function Dashboard({auth, clashUrl, unitPrice, servers, todayTraf
       user={auth.user}
       header={
         <div className="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-          </svg>
           <h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </div>
       }
