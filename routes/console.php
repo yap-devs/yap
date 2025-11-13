eeaa96642f5eac8df0fa651c01f2f56b490a6612
hldh214 <hldh214@gmail.com>
2025-11-13 09:53:56 +0900

    Change package status notification command schedule to weekends
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,4 @@
 
 \Illuminate\Support\Facades\Schedule::command('app:update-stat-command')->everyFifteenMinutes();
 \Illuminate\Support\Facades\Schedule::command('app:process-payment-command')->everyMinute();
-\Illuminate\Support\Facades\Schedule::command('app:package-status-notification-command')->daily();
+\Illuminate\Support\Facades\Schedule::command('app:package-status-notification-command')->weekends();
