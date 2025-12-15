bc418ec6335f3d53ca14aa7f42b2cdc3d9c19d19
hldh214 <hldh214@gmail.com>
2025-12-15 09:39:37 +0900

    Change package status notification command schedule to run every Monday at 2 AM
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,4 @@
 
 \Illuminate\Support\Facades\Schedule::command('app:update-stat-command')->everyFifteenMinutes();
 \Illuminate\Support\Facades\Schedule::command('app:process-payment-command')->everyMinute();
-\Illuminate\Support\Facades\Schedule::command('app:package-status-notification-command')->weekends();
+\Illuminate\Support\Facades\Schedule::command('app:package-status-notification-command')->weeklyOn(1, '02:00'); // Every Monday at 2 AM
