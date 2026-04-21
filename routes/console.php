b5277017437c4c9a268d45bfe9e2169a405444ee
hldh214 <hldh214@gmail.com>
2026-04-21 15:12:16 +0900

    Change update-stat-command schedule to run every five minutes instead of every fifteen minutes
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:update-stat-command')->everyFifteenMinutes()->withoutOverlapping();
+Schedule::command('app:update-stat-command')->everyFiveMinutes()->withoutOverlapping();
 Schedule::command('app:process-payment-command')->everyMinute()->withoutOverlapping();
 Schedule::command('app:package-status-notification-command')->weeklyOn(1, '02:00'); // Every Monday at 2 AM
